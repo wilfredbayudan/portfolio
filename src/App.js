2a11f7c77b6703edbac1e65279fe40a5dfb92d81
Wilfred <wilfredbayudan@gmail.com>
2022-03-20 16:43:02 -1000

    Basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      Navbar
+      <Routes>
+        <Route index element={<>Projects, Skills</>} />
+        <Route to="/resume" element={<>Resume</>} />
+        <Route to="/contact" element={<>Contact</>} />
+      </Routes>
+      Main Content Footer
+    </BrowserRouter>
+  );
 };
 
 export default App;
