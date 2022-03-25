826d1c229c7206452a82dec605a976758da1bb11
Wilfred <wilfredbayudan@gmail.com>
2022-03-24 23:45:53 -1000

    Skills style updates

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,6 +13,7 @@ const SkillsContainer = styled.div`
 `;
 
 const Skill = styled.div`
+  background: none;
   color: #b2b2b2;
   width: 50%;
   text-align: center;
@@ -20,9 +21,13 @@ const Skill = styled.div`
   flex-direction: column;
   margin: 5px 0;
   padding: 10px;
+  transition: 20ms ease-in-out;
   i {
     font-size: 10em;
     padding-bottom: 10px;
+    margin-bottom: 20px;
+    margin-top: 0;
+    transition: all 100ms ease-in-out;
   }
   span {
     border-top: 6px solid #b2b2b2;
@@ -30,13 +35,18 @@ const Skill = styled.div`
     padding-bottom: 0;
     font-weight: bold;
     margin-top: 10px;
+    transition: all 100ms ease-in-out;
   }
   &:hover {
     background: #33e5ce;
-    color: #505050;
+    color: #3c3c3c;
+    i {
+      margin-top: 20px;
+      margin-bottom: 0;
+    }
     span {
       border-top: none;
-      border-bottom: 6px solid #505050;
+      border-bottom: 6px solid #3c3c3c;
       padding-top: 0;
       padding-bottom: 10px;
       font-weight: bold;
@@ -67,8 +77,8 @@ const Skills = ({ skillsRef }) => {
             <span>HTML 5</span>
           </Skill>
           <Skill>
-            <SkillIcon className="devicon-html5-plain"></SkillIcon>
-            <span>HTML 5</span>
+            <SkillIcon className="devicon-css3-plain"></SkillIcon>
+            <span>CSS 3</span>
           </Skill>{" "}
           <Skill>
             <SkillIcon className="devicon-html5-plain"></SkillIcon>
