089588280d39cabc786481628c1cdc8d7e7b4076
Wilfred <wilfredbayudan@gmail.com>
2022-04-05 22:52:34 -1000

    Fixed pdf file

--- src/components/ResumeView.js
+++ src/components/ResumeView.js
@@ -1,22 +1,9 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
-import resume from "../assets/resume.pdf";
+import React from "react";
 
 const ResumeView = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
   return (
     <div style={{ height: "100%" }}>
-      <embed
-        src="https://wbayudan.com/files/resume.pdf"
-        width="100%"
-        height="100%"
-      />
+      <embed src="/files/WilfredBayudan.pdf" width="100%" height="100%" />
     </div>
   );
 };
